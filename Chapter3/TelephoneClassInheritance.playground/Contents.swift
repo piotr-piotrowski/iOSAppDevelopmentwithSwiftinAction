3ff0f1b2ad06758e7ff8f11f0e2b0175b70825cf
Piotr Piotrowski <piotr@piotrovski.com>
2017-05-26 14:29:27 +0200

    3.1.6 Class inheritance

---
+++ Chapter3/TelephoneClassInheritance.playground/Contents.swift
@@ -0,0 +1,4 @@
+//: 3.1.6 Class inheritance
+// 1) Inheritance from one class
+// 2) Sharing code can only happen between subclasses and superclasses
+// 3) sometimes it's not so clear which identity is the most relevant  to subclass
