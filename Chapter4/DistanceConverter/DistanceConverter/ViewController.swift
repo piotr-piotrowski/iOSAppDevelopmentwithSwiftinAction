8f87b465d022df7a32700bb9d34aa72c61ecb969
Piotr Piotrowski <piotr@piotrovski.com>
2017-06-06 14:25:03 +0200

    Chapter 4 - Distance Converter app - 1. Initial setup

---
+++ Chapter4/DistanceConverter/DistanceConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DistanceConverter
+//
+//  Created by Piotr Piotrowski on 06/06/2017.
+//  Copyright © 2017 Piotr Piotrowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
