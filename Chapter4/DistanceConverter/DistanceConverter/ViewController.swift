da427ae336d0be9a0c48e273bed24a06ba10387c
Piotr Piotrowski <piotr@piotrovski.com>
2017-06-07 14:43:10 +0200

    Add real values

--- Chapter4/DistanceConverter/DistanceConverter/ViewController.swift
+++ Chapter4/DistanceConverter/DistanceConverter/ViewController.swift
@@ -16,14 +16,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        milesTextField.text = "\(distance.miles)"
+        kilometersTextField.text = "\(distance.km)"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
